950155e54a480f1bd24883da0d38f376be71d9e6
Hamza Ahmed <hamzasahmed11@gmail.com>
2022-03-16 16:42:34 -0400

    added test

---
+++ TestMe2Tests/TestMe2Tests.swift
@@ -0,0 +1,32 @@
+//
+//  TestMe2Tests.swift
+//  TestMe2Tests
+//
+//  Created by admin on 3/16/22.
+//
+
+import XCTest
+
+class TestMe2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
